a36dc4ef35226fdbccf55cf9588ee2327d1c3b25
philipp <philipp>
2018-07-17 20:23:27 +0200

    Less required methods for TPlainEnum (2)

--- src/lib.rs
+++ src/lib.rs
@@ -29,12 +29,55 @@
 //!
 //! Internally, the macro generates a simple enum whose numeric values start counting at 0.
 
+#![recursion_limit="512"]
+#[macro_export]
+macro_rules! enum_seq_len {
+    ($n: expr, ) => ($n);
+    ($n: expr, $enumval: tt, $($enumvals: tt,)*) => (enum_seq_len!(($n + 1), $($enumvals,)*));
+}
+
 #[macro_use]
 mod plain_enum {
-    #[macro_export]
-    macro_rules! enum_seq_len {
-        ($n: expr, $enumval: ident,) => ($n);
-        ($n: expr, $enumval: ident, $($enumvals: ident,)*) => (enum_seq_len!(($n + 1), $($enumvals,)*));
+    macro_rules! for_each_prefix (
+        ($m:ident, [$($acc:tt,)*], []) => {
+            $m!($($acc,)*);
+        };
+        ($m:ident, [$($acc:tt,)*], [$arg0:tt, $($arg:tt,)*]) => {
+            $m!($($acc,)*);
+            for_each_prefix!($m, [$($acc,)* $arg0,], [$($arg,)*]);
+        };
+    );
+    pub trait TArrayFromFn<ArrayElement> {
+        fn array_from_fn<F>(func: F) -> Self
+            where F: FnMut(usize) -> ArrayElement;
+    }
+    macro_rules! impl_array_from_fn{($($i: tt,)*) => {
+        impl<T> TArrayFromFn<T> for [T; enum_seq_len!(0, $($i,)*)] {
+            fn array_from_fn<F>(mut func: F) -> Self
+                where F: FnMut(usize) -> T
+            {
+                [$(func($i),)*]
+            }
+        }
+    }}
+    for_each_prefix!{
+        impl_array_from_fn,
+        [0,],
+        [
+            1, 2, 3, 4, 5, 6, 7, 8, 9,
+            10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+            20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+            30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+            40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
+            50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
+            60, 61, 62, 63, 64, 65, 66, 67, 68, 69,
+            70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
+            80, 81, 82, 83, 84, 85, 86, 87, 88, 89,
+            90, 91, 92, 93, 94, 95, 96, 97, 98, 99,
+            100, 101, 102, 103, 104, 105, 106, 107, 108, 109,
+            110, 111, 112, 113, 114, 115, 116, 117, 118, 119,
+            120, 121, 122, 123, 124, 125, 126, 127, 128,
+        ]
     }
 
     use std;
@@ -85,17 +128,19 @@ mod plain_enum {
         }
         /// Creates a enum map from enum values to a type, determined by `func`.
         /// The map will contain the results of applying `func` to each enum value.
-        fn map_from_fn<F, T>(/*mut*/ func: F) -> EnumMap<Self, T>
+        fn map_from_fn<F, T>(mut func: F) -> EnumMap<Self, T>
             where F: FnMut(Self) -> T,
                   Self: TInternalEnumMapType<T>,
-        ;
+        {
+            EnumMap::from_raw(TArrayFromFn::array_from_fn(|i| func(Self::from_usize(i))))
+        }
     }
 
     /// Trait used to associated enum with EnumMap.
     /// Needed because of https://github.com/rust-lang/rust/issues/46969.
     /// TODO Rust: Once this is solved, use array directly within EnumMap.
     pub trait TInternalEnumMapType<V> : TPlainEnum {
-        type InternalEnumMapType;
+        type InternalEnumMapType : TArrayFromFn<V>;
         fn index(a: &Self::InternalEnumMapType, e: Self) -> &V;
         fn index_mut(a: &mut Self::InternalEnumMapType, e: Self) -> &mut V;
         fn iter(a: &Self::InternalEnumMapType) -> slice::Iter<V>;
@@ -151,7 +196,7 @@ mod plain_enum {
     }
 
     #[macro_export]
-    macro_rules! acc_arr {
+    macro_rules! tt {
         ($func: ident, [$($acc: expr,)*], []) => {
             [$($acc,)*]
         };
@@ -176,7 +221,7 @@ mod plain_enum {
 
                 impl TPlainEnum for $enumname {
 
-                    const SIZE : usize = enum_seq_len!(1, $($enumvals,)*);
+                    const SIZE : usize = enum_seq_len!(0, $($enumvals,)*);
 
                     fn from_usize(u: usize) -> Self {
                         use std::mem;
@@ -185,12 +230,6 @@ mod plain_enum {
                     }
                     fn to_usize(self) -> usize {
                         self as usize
-                    }
-                    fn map_from_fn<F, T>(mut func: F) -> EnumMap<$enumname, T>
-                        where F: FnMut($enumname) -> T,
-                    {
-                        use self::$enumname::*;
-                        EnumMap::from_raw(acc_arr!(func, [], [$($enumvals,)*]))
                     }
                 }
 
@@ -259,7 +298,7 @@ mod tests {
 
     #[test]
     fn test_plain_enum() {
-        assert_eq!(3, enum_seq_len!(1, E1, E2, E3,));
+        assert_eq!(3, enum_seq_len!(0, E1, E2, E3,));
         assert_eq!(3, ETest::SIZE);
     }
 
